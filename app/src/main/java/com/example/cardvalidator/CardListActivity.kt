24cc5fc80dc802ddb5dadf66e90d6d2d9ab18502
alsoAchintharya <juug22btech18805@gmail.com>
2026-04-27 13:06:21 +0530

    made stored cards preview as launcher with adding feature to add new cards.

--- app/src/main/java/com/example/cardvalidator/CardListActivity.kt
+++ app/src/main/java/com/example/cardvalidator/CardListActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cardvalidator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -22,6 +24,12 @@ class CardListActivity : AppCompatActivity() {
         adapter = CardAdapter(emptyList())
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewCards)
+
+        val addButton = findViewById<Button>(R.id.addcard)
+
+        addButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
